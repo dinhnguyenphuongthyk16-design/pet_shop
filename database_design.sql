9e8fa12fad813002a5201f00095db39f405347c4
dinhnguyenphuongthyk16-design <dinhnguyenphuongthyk16@siu.edu.vn>
2025-11-17 10:18:38 +0700

    Add contact messages with seed data

--- database_design.sql
+++ database_design.sql
@@ -275,3 +275,130 @@ GO
 
 SET IDENTITY_INSERT [dbo].[CustomerProfiles] OFF
 GO
+
+
+-- ----------------------------
+-- Table structure for ContactMessages
+-- ----------------------------
+IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[ContactMessages]') AND type IN ('U'))
+	DROP TABLE [dbo].[ContactMessages]
+GO
+
+CREATE TABLE [dbo].[ContactMessages] (
+  [MessageID] int  IDENTITY(1,1) NOT NULL,
+  [FullName] nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL,
+  [Email] nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL,
+  [Phone] nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS  NULL,
+  [Subject] nvarchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL,
+  [Message] nvarchar(2000) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL,
+  [Status] nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS DEFAULT 'New' NULL,
+  [ReplyMessage] nvarchar(2000) COLLATE SQL_Latin1_General_CP1_CI_AS  NULL,
+  [RepliedBy] int  NULL,
+  [RepliedDate] datetime  NULL,
+  [CreatedDate] datetime DEFAULT getdate() NULL
+)
+GO
+
+ALTER TABLE [dbo].[ContactMessages] SET (LOCK_ESCALATION = TABLE)
+GO
+
+
+-- ----------------------------
+-- Records of ContactMessages
+-- ----------------------------
+SET IDENTITY_INSERT [dbo].[ContactMessages] ON
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'1', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'2', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'3', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'4', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'5', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'6', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'7', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'8', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'9', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'10', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'11', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'12', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'13', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'14', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'15', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'16', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'17', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'18', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'19', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'20', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'21', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'22', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'23', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'24', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'25', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'26', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'27', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'28', N'Nguy?n Van C', N'nguyenvanc@email.com', N'0912345678', N'H?i v? s?n ph?m', N'Tôi mu?n h?i v? th?c an cho chó con, có s?n ph?m nào phù h?p không?', N'New', NULL, NULL, NULL, N'2024-01-20 09:00:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'29', N'Tr?n Th? D', N'tranthid@email.com', N'0987654321', N'Khi?u n?i', N'Tôi dã d?t hàng nhung chua nh?n du?c, có th? ki?m tra giúp tôi không?', N'Read', NULL, NULL, NULL, N'2024-01-19 15:30:00.000')
+GO
+
+INSERT INTO [dbo].[ContactMessages] ([MessageID], [FullName], [Email], [Phone], [Subject], [Message], [Status], [ReplyMessage], [RepliedBy], [RepliedDate], [CreatedDate]) VALUES (N'30', N'Lê Van E', N'levane@email.com', N'0909876543', N'Tu v?n', N'Tôi có con mèo 6 tháng tu?i, nên cho an th?c an gì?', N'Replied', NULL, NULL, NULL, N'2024-01-18 11:20:00.000')
+GO
+
+SET IDENTITY_INSERT [dbo].[ContactMessages] OFF
+GO
+
